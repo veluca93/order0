de91165b5079b479063055da4b470ff77ada7522
Luca Versari <veluca93@gmail.com>
2019-03-09 21:32:27 +0100

    Update magic_makefile.

--- config.mk
+++ config.mk
@@ -3,3 +3,8 @@ CXXFLAGS:=-O3 -Wall -std=c++17 -flto -g -march=native -I. -fno-exceptions \
 LDFLAGS:=-flto -lpthread
 CXX:=g++
 
+${TGT}bin/optimizer: ${TGT}build/optimizers/annealing.o.tar \
+	${TGT}build/optimizers/line_search.o.tar \
+	${TGT}build/optimizers/simulated_gradient.o.tar \
+	${TGT}build/optimizers/cma_es.o.tar \
+	${TGT}build/optimizers/nelder_mead.o.tar
